c3ba13410329a28af4dbc1785e561d43c0ab2b65
Srecko Kostic <45309706+srele96@users.noreply.github.com>
2026-04-26 00:13:09 +0200

    add vector, dot, cross, length

--- main.cpp
+++ main.cpp
@@ -1,124 +1,143 @@
+#include <cmath>
 #include <concepts>
 #include <cstddef>
 #include <iostream>
 #include <utility>
 
 namespace RubypointTask {
-  template <typename T>
-  concept Scalar = std::is_arithmetic_v<T>;
 
-  template<std::size_t N, Scalar T>
-  struct Vector {
-    T data[N];
+template <typename T>
+concept Scalar = std::is_arithmetic_v<T>;
 
-    T& operator[](std::size_t i) { return data[i]; }
-    const T& operator[](std::size_t i) const { return data[i]; }
+// We need vector members to be packed sequentially in memory. This allows CPU
+// and GPU efficiency, as GPU is often optimized for 16 bit alignment.
+template <std::size_t N, Scalar T>
+struct Vector {
+  T data[N];
+
+  T& operator[](std::size_t i) { return data[i]; }
+  const T& operator[](std::size_t i) const { return data[i]; }
+};
+
+// I made the Vector class as generic as possible (but not more than necessary),
+// even though the task requirements specifically say the computation is in 3D
+// space. Vector should be N dimensional container of specialized scalar types.
+template <Scalar T>
+struct Vector<3, T> {
+  // Allows access to each member for this specialization in a more clean way.
+  union {
+    T data[3];
+
+    struct {
+      T x;
+      T y;
+      T z;
+    };
   };
 
-  template<Scalar T>
-  struct Vector<3, T> {
-    union {
-      T data[3];
-      struct { T x; T y; T z; }
-    }
+  explicit Vector(T x_ = 0, T y_ = 0, T z_ = 0) : x{x_}, y{y_}, z{z_} {}
 
-    T& operator[](std::size_t i) { return data[i]; }
+  // --------------------------------------------------------------------------
+
+  T& operator[](std::size_t i) { return data[i]; }
+  const T& operator[](std::size_t i) const { return data[i]; }
+
+  // --------------------------------------------------------------------------
+
+  Vector operator+(const Vector& o) const {
+    return Vector{x + o.x, y + o.y, z + o.z};
+  }
+
+  Vector operator-(const Vector& o) const {
+    return Vector{x - o.x, y - o.y, z - o.z};
+  }
+
+  Vector operator*(const Vector& o) const {
+    return Vector{x * o.x, y * o.y, z * o.z};
+  }
+
+  Vector operator/(const Vector& o) const {
+    return Vector{x / o.x, y / o.y, z / o.z};
+  }
+
+  // --------------------------------------------------------------------------
+
+  Vector& operator+=(const Vector& o) { return *this = *this + o; }
+
+  Vector& operator-=(const Vector& o) { return *this = *this - o; }
+
+  Vector& operator*=(const Vector& o) { return *this = *this * o; }
+
+  Vector& operator/=(const Vector& o) { return *this = *this / o; }
+
+  Vector operator+(T s) const { return Vector{x + s, y + s, z + s}; }
+
+  // --------------------------------------------------------------------------
+
+  friend Vector operator+(T s, const Vector& v) { return v + s; }
+
+  Vector operator-(T s) const { return Vector{x - s, y - s, z - s}; }
+
+  friend Vector operator-(T s, const Vector& v) {
+    return Vector{s - v.x, s - v.y, s - v.z};
+  }
+
+  Vector operator*(T s) const { return Vector{x * s, y * s, z * s}; }
+
+  friend Vector operator*(T s, const Vector& v) { return v * s; }
+
+  Vector operator/(T s) const { return Vector{x / s, y / s, z / s}; }
+
+  friend Vector operator/(T s, const Vector& v) {
+    return Vector{s / v.x, s / v.y, s / v.z};
+  }
+};
+
+// ----------------------------------------------------------------------------
+
+using Vec3f = Vector<3, float>;
+
+// ----------------------------------------------------------------------------
+
+template <std::size_t N, Scalar T>
+T dot(const Vector<N, T>& a, const Vector<N, T>& b) {
+  T sum{0};
+
+  for (std::size_t i{0}; i < N; ++i) {
+    sum += a[i] * b[i];
+  }
+
+  return sum;
+}
+
+template <std::size_t N, Scalar T>
+T length(const Vector<N, T>& a) {
+  T sum{0};
+
+  for (std::size_t i{0}; i < N; ++i) {
+    sum += a[i] * a[i];
+  }
+
+  return std::sqrt(sum);
+}
+
+template <Scalar T>
+Vector<3, T> cross(const Vector<3, T>& a, const Vector<3, T>& b) {
+  return Vector<3, T>{
+      a.y * b.z - a.z * b.y,  //
+      a.z * b.x - a.x * b.z,  //
+      a.x * b.y - a.y * b.x   //
   };
+}
 
-  using Vec3f = Vector<float>;
-
-  // Use GLSL/GLM namings
-  template <Scalar T> class Vec3 {
-    // Ensure default value initialization using {}
-    T m_x{};
-    T m_y{};
-    T m_z{};
-
-  public:
-    Vec3() = default;
-
-    explicit Vec3(T value) : m_x{ value }, m_y{ value }, m_z{ value } {}
-
-    Vec3(T x, T y, T z) : m_x{ x }, m_y{ y }, m_z{ z } {}
-
-    // TODO: +=, -=, *=, /=
-    // TODO: Add friend operator overload for scalar * Vec3{}
-
-    Vec3& operator+=(const Vec3& other) {
-      m_x += other.m_x;
-      m_y += other.m_y;
-      m_z += other.m_z;
-
-      return *this;
-    }
-
-    friend Vec3 operator+(Vec3 lhs, const Vec3& rhs) {
-      lhs += rhs;
-
-      return lhs;
-    }
-
-    Vec3& operator-=(const Vec3& other) {
-      m_x -= other.m_x;
-      m_y -= other.m_y;
-      m_z -= other.m_z;
-
-      return *this;
-    }
-
-    friend Vec3 operator-(Vec3 lhs, const Vec3& rhs) {
-      lhs -= rhs;
-
-      return lhs;
-    }
-
-    Vec3& operator*=(const Vec3& other) {
-      m_x *= other.m_x;
-      m_y *= other.m_y;
-      m_z *= other.m_z;
-
-      return *this;
-    }
-
-    friend Vec3 operator*(Vec3 lhs, const Vec3& rhs) {
-      lhs *= rhs;
-
-      return lhs;
-    }
-
-    friend Vec3 operator*(T s, const Vec3& v) {
-      //
-    }
-
-    friend Vec3 operator*(const Vec3& v, T s) {
-      //
-    }
-
-    Vec3& operator/=(const Vec3& other) {
-      m_x /= other.m_x;
-      m_y /= other.m_y;
-      m_z /= other.m_z;
-
-      return *this;
-    }
-
-    friend Vec3 operator/(Vec3 lhs, const Vec3& rhs) {
-      lhs /= rhs;
-
-      return lhs;
-    }
-  };
-
-  using fVec3 = Vec3<float>;
-
-} // namespace RubypointTask
+}  // namespace RubypointTask
 
 int main() {
-  RubypointTask::fVec3 a_0;
-  RubypointTask::fVec3 a_1;
-  auto r_0{ a_1 - a_0 };
-  auto r_1{ 3.0f * a_0 };
-  auto r_2{ a_0 * 3.0f };
+  RubypointTask::Vec3f a_0;
+  RubypointTask::Vec3f a_1;
+  auto r_0{a_1 - a_0};
+  auto r_1{3.0f * a_0};
+  auto r_2{a_0 * 3.0f};
 
   std::cout << "hello world\n";
   return 0;
